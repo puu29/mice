8fdcbc5e95ff6011a4416a97b9e081bef3fd1dac
sopana <koopoopoop@gmail.com>
2020-11-27 17:56:00 +0700

    lasted

--- layout/nav.php
+++ layout/nav.php
@@ -53,7 +53,7 @@
                         <span class="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" href="index.php">
-                        <img src="assets/images/logo.svg" alt="MICE">
+                        <img src="assets/images/logo.svg?ver=1" alt="MICE">
                     </a>
                 </div>
                 <div class="collapse navbar-collapse" id="menu">
